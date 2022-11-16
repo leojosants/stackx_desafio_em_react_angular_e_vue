8c6ebcfb8df5205d1cbb2ed044e30cdaf4c1218d
Leonardo Santos <leonardojosantos@gmail.com>
2022-11-16 18:04:04 -0300

    Criado e configurado projeto

---
+++ desafio_em_vue/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Hello Worl</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ desafio_em_vue/tailwind.config.js
@@ -0,0 +1,26 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+    './src/**/*.{html, js, vue}'
+  ],
+
+  theme: {
+    extend: {},
+    colors: {
+      'cod-gray': '#121212',
+      'white': '#FFFFFF',
+      'shark': '#27272A',
+      'brown': '#9D4B00',
+      'rgba': 'rgba(218, 218, 218, 0.33)',
+    },
+
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    }
+  },
+
+  plugins: [],
+}
